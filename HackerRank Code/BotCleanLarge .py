1aaa45138539d8d5c7931ccc188b0d8cfcd17fee
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2022-08-22 18:56:05 +0500

    completing large cleaning bot

--- HackerRank Code/BotCleanLarge .py
+++ HackerRank Code/BotCleanLarge .py
@@ -1,33 +1,41 @@
 import math
 
 # Update cost that bot need to arrive the dirty
+
+
 def update_position(posr, posc, dirties):
     nearest_dirt = []
     for i in range(len(dirties)):
         # Euclidean distance
-        result = math.sqrt(((dirties[i][0] - posr) ** 2) + ((dirties[i][1] - posc) ** 2))
+        result = math.sqrt(
+            ((dirties[i][0] - posr) ** 2) + ((dirties[i][1] - posc) ** 2))
         nearest_dirt.append(result)
-    return [x for (y,x) in sorted(zip(nearest_dirt,dirties))]
+    return [x for (y, x) in sorted(zip(nearest_dirt, dirties))]
+
+# Set the bot in your new position
+
 
 def next_move(posx, posy, dimx, dimy, board):
     dirties = []
-    for i in range(len(board)):
-        for j in range(len(board[i])):
+    for i in range(dimx):
+        for j in range(dimy):
             if board[i][j] == 'd':
                 dirties.append([i, j])
 
     next_dirt = update_position(posx, posy, dirties)
-    if next_dirt[0][1] < posy:
-        print('LEFT')
-    elif next_dirt[0][1]  > posy:
-        print('RIGHT')
-    elif next_dirt[0][0] < posx:
+    if next_dirt[0][0] < posx:
         print('UP')
     elif next_dirt[0][0] > posx:
         print('DOWN')
+    elif next_dirt[0][1] < posy:
+        print('LEFT')
+    elif next_dirt[0][1] > posy:
+        print('RIGHT')
     else:
         print('CLEAN')
 
+
+# Set data
 if __name__ == "__main__":
     pos = [int(i) for i in input().strip().split()]
     dim = [int(i) for i in input().strip().split()]
