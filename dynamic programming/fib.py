4c0474dbce359556b058fd42486511b529d98703
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-01-26 19:56:05 +0500

    fibnachi using recution with omtimize time complexity

---
+++ dynamic programming/fib.py
@@ -0,0 +1,12 @@
+# memozation using dict in python
+# memozation help remove to duplicate thing in tree
+def fib(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+
+
+print(fib(100))
