3d5b759a4b80761170aed5f2894db24dafb76a6e
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-01-27 16:03:56 +0500

    grid travaler after using memoization

--- dynamic programming/gridTraveler.py
+++ dynamic programming/gridTraveler.py
@@ -1,10 +1,14 @@
-def gridTraveler(m, n):
+def gridTraveler(m, n, memo={}):
+    key = str(m) + "," + str(n)
+    if key in memo:
+        return memo[key]
     if m == 1 and n == 1:
         return 1
     if m == 0 or n == 0:
         return 0
-    return gridTraveler(m-1, n) + gridTraveler(m, n-1)
+    memo[key] = gridTraveler(m-1, n, memo) + gridTraveler(m, n-1, memo)
+    return memo[key]
 
 
-# it's not working for 18 by 18 grid becuase it's to slow
+# now this code run fast now it time complex reduce from expontent to polynolial
 print(gridTraveler(18, 18))
