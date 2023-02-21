d7f78927b832b6b3da3e81f7bc2cfe2a98a88e62
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-02-21 14:53:51 +0500

    how to find hidden file

---
+++ dynamic programming/link.py
@@ -0,0 +1,20 @@
+import os
+
+
+def find_folders(path):
+    folder_names = []
+    folder_links = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for dirname in dirnames:
+            folder_path = os.path.join(dirpath, dirname)
+            folder_names.append(dirname)
+            folder_links.append(folder_path)
+    return folder_names, folder_links
+
+
+folder_names, folder_links = find_folders("D:\\")
+
+print("Folders in D drive:")
+for i, folder in enumerate(folder_names):
+    if folder == "hidden file name":
+        print(f"{i+1}. {folder} ({folder_links[i]})")
