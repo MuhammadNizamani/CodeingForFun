4c7341b50a1257c72fe43bfa35ca04253381ce1f
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-29 17:56:19 +0500

    I need to install

--- C++/check.cpp
+++ C++/check.cpp
@@ -4,6 +4,6 @@ int main()
 {
     int me, a, c;
     cin >> me;
-    cout << "looking for idea to implemnt the RL in c++" << endl;
+    cout << "looking for idea to implemnt the RL in c++ I need to intall C++ Boost Library" << endl;
     return 0;
 }
