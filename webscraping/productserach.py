793924555b4eec145922c545ef00b805675e13cc
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-04-28 12:18:29 +0500

    finding the link for the animr for plot summary

--- webscraping/productserach.py
+++ webscraping/productserach.py
@@ -8,14 +8,15 @@ def search_product(url, product_name):
     
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(html_content, 'html.parser')
-    # print(soup)
+    #print(soup)
     # Find all the anime titles on the page
-    anime_titles = soup.find_all('a')
-    
+    anime_titles = soup.find_all('a',)
+    # print(anime_titles)
     # Search for the given product name in the list of anime titles
     for i, anime_title in enumerate(anime_titles):
         if anime_title.text.strip() == product_name:
-            return i + 1
+             
+            return str(anime_title)
     
     # If the product was not found, return None
     return None
@@ -23,9 +24,28 @@ def search_product(url, product_name):
 # Test the function with the given URL and product name
 url = "https://www3.gogoanimes.fi/anime-list-A"
 product_name = "A Day Before Us"
-product_index = search_product(url, product_name)
+html = search_product(url, product_name)
 
-if product_index:
-    print(f"The product '{product_name}' was found at index {product_index}.")
-else:
-    print(f"The product '{product_name}' was not found on the website.")
+# if product_index:
+#     print(f"The product '{product_name}' was found at index {product_index}.")
+# else:
+#     print(f"The product '{product_name}' was not found on the website.")
+
+# print(type(anime_link))
+print(html)
+
+# Sample HTML code
+# html = '<a href="/category/ai-no-wakakusa-monogatari" title="">Ai no Wakakusa Monogatari</a>'
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+# Get the `href` attribute value of the `a` tag
+href = soup.a['href']
+
+# Get the text content of the `a` tag
+text = soup.a.text
+
+# Print the `href` attribute value and text content
+print(f"href: {href}")
+print(f"text: {text}")
