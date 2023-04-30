a23950b174d2be07fd28e4a8dab69da1d3f90376
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-04-30 11:44:39 +0500

    ENH: one anime's data is can be extred now

--- webscraping/gettingdetailanime.py
+++ webscraping/gettingdetailanime.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+iswanted = False
 url = "https://www3.gogoanimes.fi//category/addiction-to-wife-doting"
 response = requests.get(url)
 html_content = response.text
@@ -10,5 +10,15 @@ soup = BeautifulSoup(html_content, 'html.parser')
 #print(soup)
 # Find all the anime titles on the page
 anime_titles = soup.find_all('p',)
+
 for i, anime_title in enumerate(anime_titles):
-    print(str(anime_title.text.strip()))
+    if "Type:" in anime_title.text.strip() :
+        iswanted = True
+    if "Episode " in anime_title.text.strip():
+        iswanted = False
+    if iswanted:
+        # print(anime_title)
+        # my_string=str(anime_title.text.strip())
+        # print("\n".join([line for line in my_string.split("\n") if line.strip()]))
+        print(anime_title.text.strip())
+        
