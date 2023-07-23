eb9feac6576db54b154576ca7704ee6c84092110
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-07-23 10:26:55 +0500

    working on yolo

--- C++/work.cpp
+++ C++/work.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+// I am working on getting yolo
 using namespace std;
 int main(){
+    int a;
     cout<<"working on checking branch "<<endl;
+    cin>>a;
+
     // TODO work
 }
